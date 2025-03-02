7f6d6366db78ea286c1aad4c7ca204a6a30e3d7c
allydevper <alamaswilmer@gmail.com>
2025-03-01 22:15:50 -0500

    change to remix

--- src/appwrite.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Account } from 'appwrite';
-
-//cambiar por privado
-const client = new Client()
-    .setEndpoint(import.meta.env.VITE_API_URL)
-    .setProject(import.meta.env.VITE_PUBLIC_KEY);
-
-const account = new Account(client);
-export { client, account };
