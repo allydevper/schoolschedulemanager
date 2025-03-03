a33082bcb2d05ec19702c6a0fae9d66d367bcd23
allydevper <alamaswilmer@gmail.com>
2025-03-03 00:13:38 -0500

    Update appwrite.ts

--- app/appwrite.ts
+++ app/appwrite.ts
@@ -6,7 +6,5 @@ const client = new Client()
     .setEndpoint(getPublicEnv().VITE_API_URL)
     .setProject(getPublicEnv().VITE_PUBLIC_KEY);
 
-console.log(getPublicEnv().VITE_API_URL)
-
 const account = new Account(client);
 export { client, account };
